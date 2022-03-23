5bdae0954b4bc287c326d7a0d66df78f7852d4e5
ethicolorr <xex.opy@gmail.com>
2022-03-23 14:17:18 +0700

    fix: edit urls

--- HealthMe/urls.py
+++ HealthMe/urls.py
@@ -1,11 +1,16 @@
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 
+from HealthMe import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('authentication.urls')),
     path('api/', include('api.urls'))
-#    path('api/auth/', include('knox.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
 
